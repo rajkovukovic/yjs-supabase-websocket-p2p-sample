d3212624809d5e36129502e09e49264fd4fb5729
Rajko Vukovic <rajkovukovic@gmail.com>
2025-10-16 12:55:05 +0100

    Step 4

--- web/types.ts
+++ web/types.ts
@@ -1,3 +1,4 @@
+import Konva from 'konva'
 import { Drawable, Comment } from './lib/schemas'
 
 export interface Rectangle {
@@ -18,6 +19,16 @@ export interface ViewBox {
   y: number
   width: number
   height: number
+}
+
+export interface DrawableProps<T extends Drawable> {
+  shapeProps: T
+  isSelected: boolean
+  onSelect: (e: Konva.KonvaEventObject<MouseEvent>) => void
+  onChange: (newAttrs: Partial<T>) => void
+  isSpacePressed: boolean
+  onDragStart?: (e: Konva.KonvaEventObject<DragEvent>) => void
+  onDragMove?: (e: Konva.KonvaEventObject<DragEvent>) => void
 }
 
 export interface DocumentState {
